1c1728d01a0953733c0ac36bc2c6a5a6ba646b80
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-08-12 20:56:37 +0545

    app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,11 +4,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
+// Allow only your frontend origin
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://looksy-omega.vercel.app", "http://localhost:5173"],
+    credentials: true,
   })
 );
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.get("/", (req, res) => {
